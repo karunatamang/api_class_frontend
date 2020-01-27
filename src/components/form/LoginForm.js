c2ae454ede7209968a602797f3c410b2c439ba88
karunatamang <korunatamang12@gmail.com>
2020-01-26 21:56:18 -0800

    form for login and register

---
+++ src/components/form/LoginForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import '../css/form.css';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+
+class LoginForm extends React.Component {
+
+    state = {
+        email: '',
+        password: '',
+        isloggedIn: false
+
+    }
+
+    handleChange = (e) => {
+        this.setState(
+            { [e.target.name]: e.target.value }
+        )
+    }
+    UserLogin = () => {
+        const data = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios.post("http://localhost:3030/login", data).then((response) => {
+                localStorage.setItem('token', response.data.token);
+                this.setState({ isloggedIn: true }).catch((err) => console.log(err.response))
+                this.setState({ email: '', password: '' })
+            
+
+            });
+    }
+
+    render() {
+        if (this.state.isloggedIn === true) {
+            return <Redirect to='/userdashboard' />
+        } else {
+            return (<section>
+                <form>
+                    <h1>Login Form</h1>
+                    <fieldset>
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" value={this.state.email}
+                            onChange={this.handleChange} required />
+
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" value={this.state.password}
+                            onChange={this.handleChange} required />
+                    </fieldset>
+
+                    <button onClick={this.UserLogin}>Login</button>
+                </form>
+            </section>
+
+            );
+
+        }
+
+
+    }
+}
+
+export default LoginForm;
+
+
---
+++ src/components/form/SignUpForm.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import '../css/form.css';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+
+class SignUpForm extends React.Component {
+
+    state = {
+        fullname: '',
+        email: '',
+        password: '',
+        address: '',
+        phonenumber: '',
+        user_data: []
+    }
+
+    registerData = () => {
+        const data = {
+            fullName: this.state.fullname,
+            email: this.state.email,
+            password: this.state.password,
+            address: this.state.address,
+            phonenumber: this.state.phonenumber,
+    
+        }
+        console.log(data);
+
+        axios.post("http://localhost:3030/register", data).then((response) => {
+            
+            return <Redirect to='/loginform' />
+        });
+
+    }
+
+    render() {
+        return (
+            <section>
+                <form>
+                    <h1>Sign Up</h1>
+                    <fieldset>
+                        <legend><span class="number">1</span>Your basic info</legend>
+                        <label for="name">Name:</label>
+                        <input type="text" value={this.state.fullname}
+                            onChange={(event) => this.setState({ fullname: event.target.value })} required />
+
+                        <label for="email">Email:</label>
+                        <input type="email" value={this.state.email}
+                            onChange={(event) => this.setState({ email: event.target.value })} required />
+
+                    
+
+                        <label for="password">Password:</label>
+                        <input type="password" value={this.state.password}
+                            onChange={(event) => this.setState({ password: event.target.value })} required />
+
+                            
+                        <label for="address">Address:</label>
+                        <input type="text" value={this.state.address}
+                            onChange={(event) => this.setState({ address: event.target.value })} required />
+
+                        <label for="phonenumber">Phone Number:</label>
+                        <input type="text" value={this.state.phonenumber}
+                            onChange={(event) => this.setState({ phonenumber: event.target.value })} required />
+                        
+
+                    </fieldset>
+                    <button onClick={this.registerData}>Sign Up</button>
+                </form>
+            </section>
+        );
+    }
+
+}
+
+export default SignUpForm;
